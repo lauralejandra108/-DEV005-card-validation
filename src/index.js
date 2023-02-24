bbc5a374b93f4c80ea311df78de9082237422c58
lauralejandra108 <118205850+lauralejandra108@users.noreply.github.com>
2023-02-24 14:27:41 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+const numsTarjeta = document.getElementById("inputNumero");
+const respuesta = document.getElementById("respuesta");
+//let validarNu = validator.IsValid(nuValidar);
+// llamar los numero ingresados
+
+const boton = document.getElementById("botton");
+boton.addEventListener("click", function(){
+  const resultadoValida = validator.isValid(numsTarjeta.value);
+  if (resultadoValida === true){ 
+    respuesta.innerHTML = "Tarjeta valida";
+    console.log("respuesta valida", resultadoValida);
+  } else { 
+    respuesta.innerHTML = "Tarjeta invalida";
+    console.log("respuesta incorrecta", resultadoValida);
+  }
+})
+numsTarjeta.addEventListener("keyup", (e)=>{
+  const numeros = e.target.value;
+  console.log(numeros);
+});
