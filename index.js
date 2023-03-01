cb2571323fd1c5a73592a526f061c199f00aff7f
lauralejandra108 <118205850+lauralejandra108@users.noreply.github.com>
2023-03-01 13:43:26 -0500

    Updates

---
+++ index.js
@@ -0,0 +1,36 @@
+import validator from './validator.js';
+
+//console.log(validator);
+const numsTarjeta = document.getElementById("inputNumero");
+const respuesta = document.getElementById("respuesta");
+// llamar los numero ingresados
+const boton = document.getElementById("botton");
+boton.addEventListener("click", function(){ 
+  const resultadoValida = validator.isValid(numsTarjeta.value);
+  if (resultadoValida === true){ 
+    respuesta.innerHTML = "Tarjeta valida";
+    //console.log("respuesta valida", resultadoValida);
+  } else { 
+    respuesta.innerHTML = "Tarjeta invalida";
+    //console.log("respuesta incorrecta", resultadoValida);
+  }
+  const encrypt = validator.maskify(numsTarjeta.value)
+  //console.log(encrypt)
+  
+  //numsTarjeta.innerHTML = encrypt
+  const button = document.getElementById("botton");
+  button.addEventListener("click", doChange);
+  function doChange() {  
+    document.getElementById('inputNumero').value = encrypt;  
+  }  
+});
+/*formulario.addEventListener("keyup", (e)=>{
+  const numTarj = e.target.value;
+  //console.log(e)
+  formulario.inputNumero.value = numTarj
+    .replace(/\s/g, "")
+    .replace(/\D/g, "") 
+    .replace(/([0-9]{4})/g, "$1 ")
+  //console.log(numsTarjeta)
+});*/
+
---
+++ validator.js
@@ -0,0 +1,60 @@
+const validator =  {
+  isValid: function (nuValidar) {
+    //console.log("estes es mi numero de entrada", nuValidar)
+    //console.log(typeof nuValidar)
+    const reveNum = nuValidar.split("").reverse(); // reversar mis numeros
+    const card = []; 
+    const sumTotal = [];
+    //console.log("split",nuValidar.split(""))
+    //console.log("reverse",nuValidar.split("").reverse())
+    //decirle que a todos los números que ocupan una posición par se les debe multiplicar por dos
+    for(let index= 0;index< reveNum.length; index++){ //comento esta linea que hace
+      //console.log(index%2)
+      if (index%2!==0) { 
+        card.push(reveNum[index]*2)
+        //console.log("impares",reveNum[index]*2)
+      }
+      else{
+        card.push(reveNum[index])
+      }
+    }
+    //console.log("Estos son los pares multipliados ", card);
+    //si este número es mayor o igual a 10, debemos sumar los dígitos del resultado
+    for(let i= 0;i< card.length; i++){
+      if(card[i]>9){
+        sumTotal.push(card[i]%10 + parseInt(card[i]/10))
+      }
+      else if(0<=card[i]<=9){
+        sumTotal.push(card[i])
+      }
+      //console.log(sumTotal);
+    }
+    let resutSum = 0;
+    //es valido si la suma de sus dígitos finales es un múltiplo de 10.
+    for (let i= 0;i< sumTotal.length; i++) {
+      const numeroConvertido=parseInt(sumTotal[i])
+      resutSum = resutSum + numeroConvertido
+      //console.log("ESTA ES LA SUMA TOTAL", resutSum) 
+    }
+    const validFinal = resutSum % 10   //es valido si la suma de sus dígitos finales es un múltiplo de 10.
+    //console.log("este es el resultado final", validFinal);   
+    if (validFinal === 0 ){  //si el total es multiplo de cero ... true
+    
+      //console.log(validFinal)
+      return true
+    } else { 
+    //console.log(validFinal)
+      return false
+    }
+  },
+  maskify: function(numValidar){
+    let nuMaski = "";
+    const cuatroDigitos = numValidar.slice(-4);
+    for (let i = 0; i < numValidar.length -4; i++ ){
+      i < numValidar.length - 4 
+      nuMaski += "#";
+    }
+    return nuMaski + cuatroDigitos; 
+  },
+};
+export default validator
