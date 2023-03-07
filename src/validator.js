5feca02ea7552075403a7e872bff42a8fe036755
lauralejandra108 <118205850+lauralejandra108@users.noreply.github.com>
2023-03-07 11:37:04 -0500

    comentarios

--- src/validator.js
+++ src/validator.js
@@ -8,24 +8,25 @@ const validator =  {
     //console.log("split",nuValidar.split(""))
     //console.log("reverse",nuValidar.split("").reverse())
     //decirle que a todos los números que ocupan una posición par se les debe multiplicar por dos
-    for(let index= 0;index< reveNum.length; index++){ //comento esta linea que hace
+    for(let index= 0;index< reveNum.length; index++){ //va al indice del array 
       //console.log(index%2)
+      //Dicir que si la posicion en diferente de cero cuando se divide por dos 
       if (index%2!==0) { 
-        card.push(reveNum[index]*2)
+        card.push(reveNum[index]*2)// multiplicar por 2
         //console.log("impares",reveNum[index]*2)
       }
       else{
-        card.push(reveNum[index])
+        card.push(reveNum[index]) //sino guardar en card
       }
     }
     //console.log("Estos son los pares multipliados ", card);
     //si este número es mayor o igual a 10, debemos sumar los dígitos del resultado
     for(let i= 0;i< card.length; i++){
       if(card[i]>9){
-        sumTotal.push(card[i]%10 + parseInt(card[i]/10))
+        sumTotal.push(card[i]%10 + parseInt(card[i]/10)) // 
       }
       else if(0<=card[i]<=9){
-        sumTotal.push(card[i])
+        sumTotal.push(card[i]) // guardar los que no son mayores a diez en variable sumtotal
       }
       //console.log(sumTotal);
     }
@@ -49,8 +50,8 @@ const validator =  {
   },
   maskify: function(numValidar){
     let nuMaski = "";
-    const cuatroDigitos = numValidar.slice(-4);
-    for (let i = 0; i < numValidar.length -4; i++ ){
+    const cuatroDigitos = numValidar.slice(-4); 
+    for (let i = 0; i < numValidar.length -4; i++ ){ //se acceden desde el 0 hasta los -4 numeros 
       i < numValidar.length - 4 
       nuMaski += "#";
     }
